d9c6c2bc43f5916180afd268f5ba1de30bfd4b2e
Michael Shervashidze <mike.fury315@gmail.com>
2025-09-07 06:52:07 +0700

    Страница с видео

---
+++ js/video.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+
+});
