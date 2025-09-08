e6a4e356e6bc5c314f79aa9e548c8c1ddf9fac9e
Michael Shervashidze <mike.fury315@gmail.com>
2025-09-08 21:10:46 +0700

    Js видео, пауза по клику, автоплей текущего слайда

--- js/video.js
+++ js/video.js
@@ -24,3 +24,30 @@ likes.forEach(like => {
   })
 })
 
+function handleVideos() {
+  const videos = document.querySelectorAll('.swiper-slide-video');
+  videos.forEach((video, index) => {
+    if (index === swiper.activeIndex) {
+      video.play().catch(() => {
+        console.log('autoplay blocked by browser');
+      });
+    } else {
+      video.pause();
+    }
+  });
+}
+
+handleVideos();
+
+swiper.on('slideChangeTransitionEnd', handleVideos);
+
+document.querySelectorAll('.swiper-slide-video').forEach(video => {
+  video.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
+
