3a9e0153b4ab98a28933673a8ad8a1075884de5e
Michael Shervashidze <mike.fury315@gmail.com>
2025-09-08 18:47:27 +0700

    Стили слайдера

--- js/video.js
+++ js/video.js
@@ -1,11 +1,16 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'vertical',
-  loop: true,
   // Navigation arrows
   navigation: {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
-
+  // slidesPerView: 'auto',  // оставляем размеры из CSS
+  // spaceBetween: 10,       // расстояние между карточками
+  // centeredSlides: true,  // не центрировать
+  watchSlidesProgress: true,
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  centeredSlides: true,
 });
